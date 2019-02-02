0a3203aa895cbbcfe8e069ca289bc71690086dad
Budi Julian <45169749+budijulian@users.noreply.github.com>
2019-02-02 22:28:20 +0700

    Hitung_Ipk

---
+++ program mencari ipk/index.py
@@ -0,0 +1,32 @@
+#  ini adalah program mencari nilai ipk sesuai matkul
+#inputan
+title = "ini adalah program mencari nilai ipk buatan budi julian dengan python"
+print(title.capitalize())
+
+Npm =input("Masukan NPM Anda : ")
+Nama =input("Masukan Nama Anda : ")
+#proses
+No =input("Masukan Jumlah Matkul Anda :")
+No1=int(No)
+
+#merupakan sebuah function
+def nilai():
+    {
+    sikap = raw_input("Masukan Nama Anda : ")
+    }
+  
+   
+for i in range(1,No1+1):
+    {
+        print("Matkul ke -", i)
+        
+    }
+nilai()
+        
+
+    
+  # memanggi function
+    
+    
+
+
